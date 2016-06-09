8c6bdc5f3f6419757012b1bc38e4a7461d11f013
Viktor <viktor.boyanov@yahoo.com>
2016-06-09 23:40:06 +0300

    Programming Fundamentals

---
+++ Programming Fundamentals/StringDictionariesLambdaLINQ/CountLettersInString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLettersInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+
+            int[] count = new int[text.Max() + 1];
+
+            foreach (char ch in text)
+            {
+                count[ch]++;
+            }
+
+            for (char ch = (char)0; ch < count.Length; ch++)
+            {
+                if(count[ch] != 0) {
+                    Console.WriteLine("{0} -> {1}", ch, count[ch]);
+                }
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/StringDictionariesLambdaLINQ/PrintAReceipt/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintAReceipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            Console.WriteLine("/" + new string('-', 22) + "\\");
+
+            double sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine("| {0,20:f2} |", nums[i]);
+                sum += Math.Round(nums[i],2);
+            }
+
+            Console.WriteLine("|" + new string('-', 22) + "|");
+            Console.WriteLine("| Total: {0,13:f2} |", sum);
+            Console.WriteLine("\\" + new string('-', 22) + "/");
+
+
+        }
+    }
+}
