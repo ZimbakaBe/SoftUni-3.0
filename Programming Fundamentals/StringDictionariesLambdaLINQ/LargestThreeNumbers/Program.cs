2f483eed508d39ac3f0ddfce465674414fa5fd10
Viktor <viktor.boyanov@yahoo.com>
2016-06-10 00:17:31 +0300

    Programming Fundamentals

---
+++ Programming Fundamentals/StringDictionariesLambdaLINQ/LargestThreeNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine().Split(' ');
+            List<int> nums = strings.Select(int.Parse).ToList();
+            var sortedNums = nums.OrderBy(x => -x).Take(3);
+
+            Console.WriteLine(string.Join(" ", sortedNums));
+        }
+    }
+}
